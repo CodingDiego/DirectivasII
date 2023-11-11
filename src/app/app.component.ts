08facc6d0470fbf8ae56bddd9627402a0675ca5d
Diego Palacios <palaciosdiegovalentino@gmail.com>
2023-11-11 07:38:56 -0300

    NEW: First Commit, OOP tryout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,25 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { Empleado } from './empleado.model';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'DirectivasII';
+  title = 'Directiva Empleados';
+
+  empleados:Empleado[]=[
+    new Empleado("Diego","Palacios","Frontend Developer", 500),
+    new Empleado("Enano","David","Chef", 220),
+    new Empleado("Bruno Luis","David","Fullstack Developer", 300),
+    new Empleado("Lolo","Rodriguez","C# Developer", 350),
+    new Empleado("Ulises","Roldan","Backend Developer", 250),
+  ]
+
 }
