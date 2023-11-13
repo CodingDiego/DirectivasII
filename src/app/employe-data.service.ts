70cc2b98d1fba819b946f8016eef35a4c4863ed8
Diego Palacios <palaciosdiegovalentino@gmail.com>
2023-11-13 10:50:06 -0300

    END: Angular Learning

--- src/app/employe-data.service.ts
+++ src/app/employe-data.service.ts
@@ -3,22 +3,51 @@ import { Empleado } from './empleado.model';
 import { EmployeeServiceService } from './employee-service.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class EmployeDataService {
-
-  empleados:Empleado[]=[
-    new Empleado("Diego","Palacios","Frontend Developer", 500),
-    new Empleado("Enano","David","Chef", 220),
-    new Empleado("Bruno Luis","David","Fullstack Developer", 300),
-    new Empleado("Lolo","Rodriguez","C# Developer", 350),
-    new Empleado("Ulises","Roldan","Backend Developer", 250),
+  empleados: Empleado[] = [
+    new Empleado('Diego', 'Palacios', 'Frontend Developer', 500),
+    new Empleado('Enano', 'David', 'Chef', 220),
+    new Empleado('Bruno Luis', 'David', 'Fullstack Developer', 300),
+    new Empleado('Lolo', 'Rodriguez', 'C# Developer', 350),
+    new Empleado('Ulises', 'Roldan', 'Backend Developer', 250),
   ];
 
-  addEmployeeService(empleado:Empleado){
-    this.ServicioVentanaEmergente.showPrompt("Persona que se va a agregar:" + "\n" + empleado.nombre + "\n" + "Salario de " + empleado.salario);
+  addEmployeeService(empleado: Empleado) {
+    this.ServicioVentanaEmergente.showPrompt(
+      'Persona que se va a agregar:' +
+        '\n' +
+        empleado.nombre +
+        '\n' +
+        'Salario de ' +
+        empleado.salario,
+    );
     this.empleados.push(empleado);
   }
+  getEmployee(index: number) {
+    let empleado: Empleado = this.empleados[index];
+    return empleado;
+  }
 
-  constructor(private ServicioVentanaEmergente:EmployeeServiceService ) { }
+  updateEmployee(id: number, uEmployeed: Empleado) {
+    if (
+      uEmployeed.nombre &&
+      uEmployeed.apellido &&
+      uEmployeed.cargo &&
+      uEmployeed.salario
+    ) {
+      this.empleados[id].nombre = uEmployeed.nombre;
+      this.empleados[id].apellido = uEmployeed.apellido;
+      this.empleados[id].cargo = uEmployeed.cargo;
+      this.empleados[id].salario = uEmployeed.salario;
+    } else alert('Fields cannot be empty');
+  }
+
+  deleteEmplpoyee(id: number) {
+    this.empleados.splice(id, 1);
+    console.log(this.empleados);
+  }
+
+  constructor(private ServicioVentanaEmergente: EmployeeServiceService) {}
 }
--- src/app/update-form/update-form.component.ts
+++ src/app/update-form/update-form.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Empleado } from '../empleado.model';
 import { FormsModule } from '@angular/forms';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { EmployeDataService } from '../employe-data.service';
+import { EmployeeServiceService } from '../employee-service.service';
 
 @Component({
   selector: 'app-update-form',
@@ -10,8 +12,26 @@ import { Router } from '@angular/router';
   imports: [CommonModule, FormsModule],
   templateUrl: './update-form.component.html',
 })
-export class UpdateFormComponent {
-  constructor(private router: Router) {}
+export class UpdateFormComponent implements OnInit {
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private DataServie: EmployeDataService,
+    private EmpleadosServie: EmployeeServiceService,
+  ) {}
+
+  ngOnInit(): void {
+    this.empleados = this.DataServie.empleados;
+
+    this.id = this.route.snapshot.params['id'];
+
+    let empleado: Empleado = this.DataServie.getEmployee(this.id);
+
+    this.cuadroNombre = empleado.nombre;
+    this.cuadroApellido = empleado.apellido;
+    this.cuadroCargo = empleado.cargo;
+    this.cuadroSalario = empleado.salario;
+  }
 
   empleados: Empleado[] = [];
 
@@ -20,9 +40,29 @@ export class UpdateFormComponent {
   cuadroCargo: string = '';
   cuadroSalario: number = 0;
 
-  editarEmpleado() {}
+  editarEmpleado() {
+    let uEmployeed = new Empleado(
+      this.cuadroNombre,
+      this.cuadroApellido,
+      this.cuadroCargo,
+      this.cuadroSalario,
+    );
+    try {
+      this.DataServie.updateEmployee(this.id, uEmployeed);
+      this.goHome();
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  deleteEmployee(id: number) {
+    this.DataServie.deleteEmplpoyee(id);
+    this.router.navigate(['']);
+  }
 
   goHome() {
     this.router.navigate(['']);
   }
+
+  id: number;
 }
